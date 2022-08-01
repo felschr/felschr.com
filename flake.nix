0452e8fa246d239a45629478da115a3f5499e576
Felix Schröter <dev@felschr.com>
2022-08-01 18:33:47 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in rec {
+        devShells = {
+          default = pkgs.mkShell {
+            name = "env";
+            buildInputs = with pkgs; [
+              nodejs-16_x
+
+              # linters & formatters
+              nixfmt
+              nodePackages.prettier
+
+              # language servers
+              nodePackages.bash-language-server
+              nodePackages.typescript-language-server
+              nodePackages.vscode-langservers-extracted
+            ];
+
+            shellHook = ''
+              export PATH="$PWD/node_modules/.bin:$PATH"
+            '';
+          };
+        };
+      });
+}
